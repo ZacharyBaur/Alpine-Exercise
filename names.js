2f60ffb34f87efad8be4dea5c2c61307878d192d
Zachary Baur <zachary.baur@gmail.com>
2026-02-01 16:04:55 +0000

    created names.js, created nameGenerator function

---
+++ names.js
@@ -0,0 +1,22 @@
+function nameGenerator() {
+    return {
+        names: [
+            "Zachary",
+            "Jack",
+            "Nolan",
+            "Suzy",
+            "Sarah",
+            "Roxy",
+            "Joe",
+            "Eric",
+            "Karli",
+            "Amelia"
+        ],
+        currentName: "",
+    }
+
+    getRandomName() {
+        const randomIndex = Math.floor(Math.random() * this.name.length);
+        this.currentName = this.names[randomIndex];
+    }
+}
